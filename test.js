69d0179ba05d6ec8cf68ff244373511b6ab0bf79
Ilya <158504500+Beyyoonddeath@users.noreply.github.com>
2024-05-08 16:33:22 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,19 +1,19 @@
-// методы сериализации и десериализации
+// serialization and deserialization methods
 const ser = arr => JSON.stringify(arr),
     deser = str => JSON.parse(str);
 
-// функция получения рандомного натурального числа
+// getting random digit by min and max values
 function getRandomArbitrary(min, max) {
   return Math.random() * (max - min) + min;
 }
 
-// создание массивов под группы чисел для тестинга
+// making arrays
 let random50 = [], random100 = [], random500 = [], random1000 = [];
 let singleDigs = [], doubleDigs = [], threeDigs = [], tripled = [];
 
 let i;
 
-// заполнение этих массивов
+// filling arrays
 i=0; for (;i<50;i++) random50.push(getRandomArbitrary(1, 300));
 i=0; for (;i<100;i++) random100.push(getRandomArbitrary(1, 300));
 i=0; for (;i<500;i++) random500.push(getRandomArbitrary(1, 300));
@@ -24,9 +24,9 @@ i=10; for (;i<100;i++) doubleDigs.push(i);
 i=100; for (;i<1000;i++) threeDigs.push(i);
 i=0; for (;i<300;i++) tripled.push(i, i, i);
 
-// тесты
+// tests
 
-// сериализация
+// serialization
 ser_random50   = ser(random50)
 ser_random500  = ser(random500)
 ser_random100  = ser(random100)
@@ -36,7 +36,7 @@ ser_doubleDigs = ser(doubleDigs)
 ser_threeDigs  = ser(threeDigs)
 ser_tripled    = ser(tripled)
 
-// десериализация
+// deserialization
 deser_random50   = deser(ser_random50)
 deser_random500  = deser(ser_random500)
 deser_random100  = deser(ser_random100)
